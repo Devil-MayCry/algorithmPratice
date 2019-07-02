14c9d45b0484c4e6c52bcc091fa090d73664faae
huteng <huteng@luojilab.com>
2019-07-02 16:50:24 +0800

    feat: add 2.9

--- 2.9/main.go
+++ 2.9/main.go
@@ -6,6 +6,73 @@ rand指针是Node类中新增的指针，
 */
 package main
 
-func main() {
+import "fmt"
 
+type Node struct {
+	V    int64
+	Next *Node
+	Rand *Node
+}
+
+func makeChain() *Node {
+	c := &Node{
+		V: 3,
+	}
+	b := &Node{
+		V:    2,
+		Next: c,
+	}
+	a := &Node{
+		V:    1,
+		Next: b,
+	}
+	c.Rand = b
+	b.Rand = a
+	return a
+}
+
+func copyChain(h *Node) *Node {
+	var p, next *Node
+	p = h
+	for p != nil {
+		next = p.Next
+		newNode := &Node{
+			V: p.V,
+		}
+		p.Next = newNode
+		newNode.Next = next
+		p = next
+	}
+
+	p = h
+	for p != nil {
+		if p.Rand != nil {
+			newRand := p.Rand.Next
+			p.Next.Rand = newRand
+		}
+		p = p.Next.Next
+	}
+
+	p = h
+	newH := p.Next
+	for p != nil && p.Next != nil {
+		next = p.Next
+		p.Next = p.Next.Next
+		p = next
+	}
+	return newH
+}
+
+func printChain(h *Node) {
+	for h != nil {
+		fmt.Println(h.V)
+		fmt.Println(h.Rand)
+		h = h.Next
+	}
+}
+
+func main() {
+	h := makeChain()
+	newC := copyChain(h)
+	printChain(newC)
 }
