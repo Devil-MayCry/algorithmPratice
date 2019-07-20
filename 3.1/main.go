467896c5a43b8301503b80b2b682a303fd0f69c9
huteng <huteng@luojilab.com>
2019-07-20 14:18:45 +0800

    feat: add 3.1

--- 3.1/main.go
+++ 3.1/main.go
@@ -6,15 +6,24 @@ package main
 
 import (
 	"github.com/Devil-MayCry/algorithmPratice/lib/tree"
+	"github.com/golang-collections/collections/stack"
 )
+
+var s *stack.Stack
 
 func main() {
 	t := tree.MakeExampleTree()
-	recursivePreTraverse(t)
-	println("--------------")
-	recursiveInTraverse(t)
-	println("--------------")
-	recursivePostTraverse(t)
+	// recursivePreTraverse(t)
+	// println("--------------")
+	// recursiveInTraverse(t)
+	// println("--------------")
+	// recursivePostTraverse(t)
+	// println("--------------")
+	// recursiveInNotTraverse
+	// println("--------------")
+	// recursivePostNotTraverse(t)
+	// println("--------------")
+	recursivePostNotTraverseOneStack(t)
 }
 
 // 递归先序
@@ -28,6 +37,84 @@ func recursivePreTraverse(t *tree.Node) {
 	}
 	if t.Right != nil {
 		recursivePreTraverse(t.Right)
+	}
+}
+
+func recursivePreNotTraverse(t *tree.Node) {
+	s = stack.New()
+	if t == nil {
+		return
+	}
+	s.Push(t)
+	for s.Len() > 0 {
+		n := s.Pop().(*tree.Node)
+		println(n.V)
+		if n.Right != nil {
+			s.Push(n.Right)
+		}
+		if n.Left != nil {
+			s.Push(n.Left)
+		}
+	}
+}
+
+func recursiveInNotTraverse(t *tree.Node) {
+	s = stack.New()
+	if t == nil {
+		return
+	}
+	head := t
+	for s.Len() > 0 || head != nil {
+		if head != nil {
+			s.Push(head)
+			head = head.Left
+		} else {
+			head = s.Pop().(*tree.Node)
+			println(head.V)
+			head = head.Right
+		}
+	}
+}
+
+func recursivePostNotTraverse(t *tree.Node) {
+	s1 := stack.New()
+	s2 := stack.New()
+	if t == nil {
+		return
+	}
+	s1.Push(t)
+	for s1.Len() > 0 {
+		n := s1.Pop().(*tree.Node)
+		s2.Push(n)
+		if n.Left != nil {
+			s1.Push(n.Left)
+		}
+		if n.Right != nil {
+			s1.Push(n.Right)
+		}
+	}
+	for s2.Len() > 0 {
+		println(s2.Pop().(*tree.Node).V)
+	}
+}
+
+func recursivePostNotTraverseOneStack(h *tree.Node) {
+	s := stack.New()
+	if h == nil {
+		return
+	}
+	s.Push(h)
+	var c *tree.Node
+	for s.Len() > 0 {
+		c = s.Peek().(*tree.Node)
+		if c.Left != nil && h != c.Left && h != c.Right {
+			s.Push(c.Left)
+		} else if c.Right != nil && h != c.Right {
+			s.Push(c.Right)
+		} else {
+			println(s.Pop().(*tree.Node).V)
+			h = c
+		}
 	}
 }
 
